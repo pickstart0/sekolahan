08ca2cd159b9ba622a4711bd619e989f6c612b90
pickstart0 <144678620+pickstart0@users.noreply.github.com>
2025-02-19 19:32:18 +0800

    Add files via upload

---
+++ pemograman dasar/remed/furniture/landing/dummy.php
@@ -0,0 +1,31 @@
+<?php
+$produklist =  [
+    [
+        "id" => 1,
+        "nama" => "Sofa ",
+        "deskripsi" => "Sudah termasuk dengan sofa dan meja",
+        "harga" => 3000000,
+        "gambar" => "sofa.jpg"
+    ],
+    [
+        "id" => 2,
+        "nama" => "Lemari",
+        "deskripsi" => "Lemari dengan fitur keamanan jangkar dan buka kunci",
+        "harga" => 2500000,
+        "gambar" => "lemari.jpg"
+    ],
+    [
+        "id" => 3,
+        "nama" => "kasur",
+        "deskripsi" => "Sudah termasuk dengan bantal dan guling",
+        "harga" => 4500000,
+        "gambar" => "kasur.jpg"
+    ],
+    [
+        "id" => 4,
+        "nama" => "Lampu Hias",
+        "deskripsi" => "Sudah termasuk dengan pasang",
+        "harga" => 3500000,
+        "gambar" => "lampuhias.jpg"
+    ]
+];
---
+++ pemograman dasar/remed/ipstore/landing/dummy.php
@@ -0,0 +1,45 @@
+<?php
+$produklist =  [
+    [
+        "id" => 1,
+        "nama" => "iphone 14 pro max",
+        "deskripsi" => "Biru 128 GB",
+        "harga" =>  16125000,
+        "gambar" => "iphone14promax.jpg"
+    ],
+    [
+        "id" => 2,
+        "nama" => "iphone 11",
+        "deskripsi" => "putih 128 GB",
+        "harga" => 5000000,
+        "gambar" => "iphone11.jpg"
+    ],
+    [
+        "id" => 3,
+        "nama" => "iphone 12 pro",
+        "deskripsi" => "black 128 GB",
+        "harga" => 10500000,
+        "gambar" => "iphone12pro.jpg"
+    ],
+    [
+        "id" => 4,
+        "nama" => "iphone 14 pro",
+        "deskripsi" => "biru 128 GB",
+        "harga" => 14125000,
+        "gambar" => "iphone14pro.jpg"
+    ],
+    [
+        "id" => 5,
+        "nama" => "iphone 11 pro",
+        "deskripsi" => "gray 128 GB",
+        "harga" => 6000000,
+        "gambar" => "iphone11pro.jpg"
+    ],
+    [
+        "id" => 6,
+        "nama" => "iphone 12 pro max",
+        "deskripsi" => "white 256 GB",
+        "harga" => 11500000,
+        "gambar" => "iphone12promax.jpg"
+    ]
+];
