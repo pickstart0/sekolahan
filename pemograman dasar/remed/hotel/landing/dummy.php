91d7a41238b785077df87fca0ff95dd6da32e128
pickstart0 <144678620+pickstart0@users.noreply.github.com>
2025-03-06 12:06:01 +0700

    Add files via upload

---
+++ pemograman dasar/remed/hotel/landing/dummy.php
@@ -0,0 +1,21 @@
+<?php
+$produk = [
+    [
+        "nama" => "Kamar Standar",
+        "harga" => 500000,
+        "deskripsi" => "Kamar yang nyaman dengan fasilitas dasar.",
+        "gambar" => "standart.jpg"
+    ],
+    [
+        "nama" => "Kamar Deluxe",
+        "harga" => 800000,
+        "deskripsi" => "Kamar yang lebih luas dengan fasilitas premium.",
+        "gambar" => "duluxe.jpg"
+    ],
+    [
+        "nama" => "Kamar Executif",
+        "harga" => 1200000,
+        "deskripsi" => "Kamar yang lebih luas dengan fasilitas premium.",
+        "gambar" => "executif.jpg"
+    ]
+];
